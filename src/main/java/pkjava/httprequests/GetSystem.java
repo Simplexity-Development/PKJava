65ec73e9cb76d84e2d1decbc30cd71abe30e5ded
RhythmicSys <mc.adhdmc@gmail.com>
2023-07-01 19:57:22 -0700

    more work

--- src/main/java/pkjava/httprequests/GetSystem.java
+++ src/main/java/pkjava/httprequests/GetSystem.java
@@ -33,14 +33,6 @@ public class GetSystem {
                 .build();
         HttpResponse<String> requestResponse = client.send(systemRequest, HttpResponse.BodyHandlers.ofString());
         JSONObject requestBody = HTTP.toJSONObject(requestResponse.body());
-        JSONArray systemPrivacyJSON = requestBody.getJSONArray("privacy");
-        SystemPrivacy systemPrivacy = new SystemPrivacy();
-        systemPrivacy.setDescription_privacy(systemPrivacyJSON.getString(0));
-        systemPrivacy.setPronoun_privacy(systemPrivacyJSON.getString(1));
-        systemPrivacy.setMember_list_privacy(systemPrivacyJSON.getString(2));
-        systemPrivacy.setGroup_list_privacy(systemPrivacyJSON.getString(3));
-        systemPrivacy.setFront_privacy(systemPrivacyJSON.getString(4));
-        systemPrivacy.setFront_history_privacy(systemPrivacyJSON.getString(5));
         SystemObject requestedSystem = new SystemObject();
         requestedSystem.setId(requestBody.getString("id"));
         requestedSystem.setUuid(UUID.fromString(requestBody.getString("uuid")));
@@ -52,7 +44,23 @@ public class GetSystem {
         requestedSystem.setBanner(requestBody.getString("banner"));
         requestedSystem.setColor(requestBody.getString("color"));
         requestedSystem.setCreated(requestBody.getString("created"));
-        requestedSystem.setPrivacy(systemPrivacy);
+        requestedSystem.setPrivacy(getSystemPrivacyFromBody(requestBody));
         return requestedSystem;
+    }
+    
+    private SystemPrivacy getSystemPrivacyFromBody(JSONObject requestBody){
+        String privacyString = requestBody.getString("privacy");
+        if (privacyString == null || privacyString.isEmpty()) {
+            return null;
+        }
+        JSONArray systemPrivacyJSON = requestBody.getJSONArray("privacy");
+        SystemPrivacy systemPrivacy = new SystemPrivacy();
+        systemPrivacy.setDescription_privacy(systemPrivacyJSON.getString(0));
+        systemPrivacy.setPronoun_privacy(systemPrivacyJSON.getString(1));
+        systemPrivacy.setMember_list_privacy(systemPrivacyJSON.getString(2));
+        systemPrivacy.setGroup_list_privacy(systemPrivacyJSON.getString(3));
+        systemPrivacy.setFront_privacy(systemPrivacyJSON.getString(4));
+        systemPrivacy.setFront_history_privacy(systemPrivacyJSON.getString(5));
+        return systemPrivacy;
     }
 }
